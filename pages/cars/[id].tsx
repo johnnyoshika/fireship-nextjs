9134afcc825daade6c6ad9b8dcba5584a432c9e6
Johnny Oshika <johnnyoshika@gmail.com>
2021-12-03 23:55:46 -0800

    Use static generation for car page

--- pages/cars/[id].tsx
+++ pages/cars/[id].tsx
@@ -1,10 +1,50 @@
-import type { NextPage } from 'next';
+import {
+  GetServerSideProps,
+  GetStaticPaths,
+  GetStaticProps,
+} from 'next';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 
-const Car: NextPage = () => {
+// Static Generation (SSG)
+
+export const getStaticProps: GetStaticProps = async context => {
+  const req = await fetch(
+    `http://localhost:3000/${context.params.id}.json`,
+  );
+  const data = await req.json();
+
+  return {
+    props: { car: data },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const req = await fetch(`http://localhost:3000/cars.json`);
+  const data = await req.json();
+
+  const paths = data.map((car: string) => ({
+    params: { id: car },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+const Car = ({ car }: { car: any }) => {
   const router = useRouter();
   const { id } = router.query;
-  return <div>Hello {id}</div>;
+  return (
+    <>
+      <Head>
+        <title>{car.color}</title>
+      </Head>
+      <h1>Hello {id}</h1>
+      <img src={car.image} alt={car.id} />
+    </>
+  );
 };
 
 export default Car;
