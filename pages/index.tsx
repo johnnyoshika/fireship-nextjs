207cbb48ce51e157e27b475fc2ffd6059326245b
Johnny Oshika <johnnyoshika@gmail.com>
2021-12-04 11:36:44 -0800

    Install and set up react-hot-toast

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
 import type { NextPage } from 'next';
+import toast from 'react-hot-toast';
 
 const Home: NextPage = () => {
-  return <div>Hello world!</div>;
+  return (
+    <div>
+      <button onClick={() => toast.success('Success')}>
+        Success
+      </button>
+    </div>
+  );
 };
 
 export default Home;
