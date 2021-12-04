b09ece03ffd3b126a27ea17c6e22c939caf1cfc5
Johnny Oshika <johnnyoshika@gmail.com>
2021-12-03 23:56:49 -0800

    Use server-side rendering for car page

--- pages/cars/[id].tsx
+++ pages/cars/[id].tsx
@@ -1,37 +1,20 @@
-import {
-  GetServerSideProps,
-  GetStaticPaths,
-  GetStaticProps,
-} from 'next';
+import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 
-// Static Generation (SSG)
+// Server-Side Rendering (SSR)
 
-export const getStaticProps: GetStaticProps = async context => {
-  const req = await fetch(
-    `http://localhost:3000/${context.params.id}.json`,
-  );
-  const data = await req.json();
+export const getServerSideProps: GetServerSideProps =
+  async context => {
+    const req = await fetch(
+      `http://localhost:3000/${context.params?.id}.json`,
+    );
+    const data = await req.json();
 
-  return {
-    props: { car: data },
+    return {
+      props: { car: data },
+    };
   };
-};
-
-export const getStaticPaths: GetStaticPaths = async () => {
-  const req = await fetch(`http://localhost:3000/cars.json`);
-  const data = await req.json();
-
-  const paths = data.map((car: string) => ({
-    params: { id: car },
-  }));
-
-  return {
-    paths,
-    fallback: false,
-  };
-};
 
 const Car = ({ car }: { car: any }) => {
   const router = useRouter();
